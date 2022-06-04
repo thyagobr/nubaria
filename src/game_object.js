25a3098896b652457911a5057b0ac59ba2dcc134
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2022-06-04 14:45:20 +0200

    100% screen

--- src/game_object.js
+++ src/game_object.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d')
 function GameObject() {
   this.canvas = canvas
   this.canvas_rect = canvas.getBoundingClientRect()
+  this.canvas.setAttribute('width', this.canvas_rect.width);
+  this.canvas.setAttribute('height', this.canvas_rect.height);
   this.ctx = ctx
   this.tile_size = 20
   this.creeps = []
--- src/screen.js
+++ src/screen.js
@@ -1,10 +1,10 @@
 function Screen(go) {
   this.go = go
   this.go.screen = this
-  this.background_image = new Image()
-  this.background_image.src = "background_city.jpeg"
-  this.width  = 1080
-  this.height = 1920
+  //this.background_image = new Image()
+  //this.background_image.src = "background_city.jpeg"
+  this.width  = this.go.canvas_rect.width;
+  this.height = this.go.canvas_rect.height;
 
   this.clear = () => {
     this.go.ctx.clearRect(0, 0, this.go.canvas.width, this.go.canvas.height);
@@ -20,6 +20,9 @@ function Screen(go) {
         this.go.canvas_rect.height,
         0, 0,
         this.go.canvas_rect.width, this.go.canvas_rect.height)
+    } else {
+      this.go.ctx.fillStyle = "purple"
+      this.go.ctx.fillRect(0, 0, this.go.canvas.width, this.go.canvas.height)
     }
   }
 
