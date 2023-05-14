ca3da7267b5bf00d4de9b99bc99ebfa969000c50
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2023-05-14 15:39:41 +0200

    Projectile trace

--- src/projectile.js
+++ src/projectile.js
@@ -1,5 +1,4 @@
 import Particle from "./particle.js"
-import Position from "./position.js";
 import { Vector2, random } from "./tapete.js";
 
 export default function Projectile({ go, subject }) {
@@ -9,6 +8,7 @@ export default function Projectile({ go, subject }) {
     this.current_position = null
     this.end_position = null
     this.subject = subject
+    this.trace_count = 7
     this.bounds = () => {
         return { ...this.current_position, width: 5, height: 5 }
     }
@@ -20,27 +20,35 @@ export default function Projectile({ go, subject }) {
         this.current_position = Object.create(this.start_position)
         this.end_position = end_position
         this.active = true
+        this.trace = [];
     }
 
     this.update = () => {
         if (Vector2.distance(this.end_position, this.current_position) < 5) {
-            this.active = false;
             this.subject.end();
+            this.end();
             return;
         }
 
         this.calculate_position();
+        this.trace.push(Object.create(this.current_position))
+        this.trace = this.trace.slice(-1 * this.trace_count)
     }
 
     this.draw = () => {
         if (!this.active) return;
 
         this.particle.draw(this.current_position);
+        this.trace.forEach(traced_position => this.particle.draw(traced_position))
+    }
+
+    this.end = () => {
+        this.active = false;
     }
 
     this.calculate_position = () => {
         const angle = Vector2.angle(this.current_position, this.end_position);
-        const speed = 10;
+        const speed = random(3, 12);
         this.current_position = {
             x: this.current_position.x + speed * Math.cos(angle),
             y: this.current_position.y + speed * Math.sin(angle)
