52d3673549de834c96e74dc4fde58ac6b1209995
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2023-05-18 23:23:35 +0200

    Battle text

--- src/behaviors/stats.js
+++ src/behaviors/stats.js
@@ -15,6 +15,7 @@ export default function Stats({ go, entity, hp = 100, current_hp, mana, current_
     this.is_dead = () => this.current_hp <= 0;
     this.is_alive = () => !this.is_dead();
     this.take_damage = ({ damage }) => {
+        new ScrollDamageText({ go: this.go, entity: this.entity, damage }).spawn()
         this.current_hp -= damage;
         if (this.is_dead()) this.die()
     }
@@ -36,5 +37,42 @@ export default function Stats({ go, entity, hp = 100, current_hp, mana, current_
             target.stats.take_damage({ damage: damage })
             this.last_attack_at = Date.now();
         }
+    }
+
+    function ScrollDamageText({ go, entity, damage }) {
+        this.go = go;
+        this.entity = entity;
+        this.damage = damage;
+        this.active = false
+        this.starting_time = null
+        this.display_time = 2000
+        this.font_size = 21
+        this.x = this.entity.x + (random(0, this.entity.width)) - this.go.camera.x
+        this.y = this.entity.y - this.go.camera.x
+
+        this.spawn = () => {
+            this.starting_time = Date.now();
+            this.active = true
+            this.go.managed_objects.push(this);
+        }
+
+        this.draw = () => {
+            this.go.ctx.fillStyle = 'white'
+            this.go.ctx.font = `${this.font_size}px sans-serif`
+            let text = `${this.damage}`
+            var text_measurement = this.go.ctx.measureText(text)
+            this.go.ctx.fillText(text, this.x, this.entity.y - this.go.camera.y)
+        }
+
+        this.update = () => { 
+            if (this.active && Date.now() > this.starting_time + this.display_time) {
+                this.active = false
+                remove_object_if_present(this, this.go.managed_objects)
+            }
+            this.font_size += 0.2
+            this.y -= 0.2
+        }
+        this.update_fps = () => { }
+        this.end = () => { }
     }
 }
